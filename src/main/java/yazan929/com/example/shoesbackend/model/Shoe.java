ad8ccd42f6da4dbdfdfca60b834471bc9559efda
Yazan Qawasmeh <yazan929@users.noreply.github.com>
2021-04-17 01:28:59 +0800

    working backend

--- src/main/java/yazan929/com/example/shoesbackend/model/Shoe.java
+++ src/main/java/yazan929/com/example/shoesbackend/model/Shoe.java
@@ -3,24 +3,28 @@ package yazan929.com.example.shoesbackend.model;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
 
-@Document(collation = "shoe")
+// @Document(collation="shoe")
+
+@Document(collection = "shoe")
 public class Shoe {
-    
+
     @Id
     private long id;
     private String name;
     private String msg;
-    
-    public Shoe(){
+    private String photo;
+
+    public Shoe() {
         super();
     }
 
-    public Shoe(String name, String msg){
+    public Shoe(String name, String msg) {
         super();
         this.name = name;
         this.msg = msg;
 
     }
+
     public long getId() {
         return this.id;
     }
@@ -37,6 +41,14 @@ public class Shoe {
         this.name = name;
     }
 
+    public void setPhoto(String photo) {
+        this.photo = photo;
+    }
+
+    public String getPhoto() {
+        return this.photo;
+    }
+
     public String getMsg() {
         return this.msg;
     }
@@ -44,6 +56,5 @@ public class Shoe {
     public void setMsg(String msg) {
         this.msg = msg;
     }
-
 
 }
