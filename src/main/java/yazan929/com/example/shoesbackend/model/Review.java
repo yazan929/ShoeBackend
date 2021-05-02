731da3583d36a47e7f44fa687402a0a2e5aa6d53
Yazan Qawasmeh <yazan929@users.noreply.github.com>
2021-05-02 09:54:32 +0800

    reviews now has name stars email and others

--- src/main/java/yazan929/com/example/shoesbackend/model/Review.java
+++ src/main/java/yazan929/com/example/shoesbackend/model/Review.java
@@ -10,15 +10,31 @@ public class Review {
     @Id
     private long id;
     private String text;
+    private String email;
+    private String name;
+    private Integer stars;
+    private String shoeName;
+
+    public String getShoeName() {
+        return this.shoeName;
+    }
+
+    public void setShoeName(String shoeName) {
+        this.shoeName = shoeName;
+    }
 
 
     public Review() {
         super();
     }
 
-    public Review(String text) {
+    public Review(String text,String email,String name , Integer stars, String shoeName ) {
         super();
+        this.shoeName = shoeName;
         this.text = text;
+        this.email = email;
+        this.name = name;
+        this.stars = stars;
 
     }
 
@@ -36,6 +52,30 @@ public class Review {
 
     public void setText(String text) {
         this.text = text;
+    }
+
+    public String getEmail() {
+        return this.email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getName() {
+        return this.name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Integer getStars() {
+        return this.stars;
+    }
+
+    public void setStars(Integer stars) {
+        this.stars = stars;
     }
 
 
